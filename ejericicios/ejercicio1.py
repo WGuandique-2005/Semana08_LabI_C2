8f8d490c03f3bc526a35fd3a914ecdfd09eb0976
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-09-14 10:44:29 -0600

    last commit

--- ejericicios/ejercicio1.py
+++ ejericicios/ejercicio1.py
@@ -8,7 +8,9 @@ todo\ usando PyQt5
 
 # se importan los módulos necesarios para crear una aplicación gráfica con PyQt5
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QLabel, QVBoxLayout, QWidget)
+from PyQt5.QtCore import Qt
+
 
 # se define la clase que hereda de QMainWindow
 
@@ -20,14 +22,22 @@ class MyApp(QMainWindow):
         self.setGeometry(100, 100, 300, 275)
         self.setWindowTitle("Ejercicio 1")
         
+        center = QWidget()
+        layout = QVBoxLayout()
+        
         # Esta etiqueta se utilizará para mostrar el nombre y la edad en la ventana
         label = QLabel("William Josue Guandique Rivera, 18 años")
         
+        # Se centra la etiqueta
+        layout.addWidget(label)
+        layout.setAlignment(Qt.AlignCenter)
+        
         # Se establece la etiqueta creada como el widget central de la ventana principal utilizando el método setCentralWidget
-        self.setCentralWidget(label)
+        center.setLayout(layout)
+        self.setCentralWidget(center)
 
 # se muestra la aplicacion
 app = QApplication(sys.argv)
 window = MyApp()
 window.show()
-app.exec()
+app.exec_()
