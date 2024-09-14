6ef0abb7e5e6729812ee66f97027b3e62d02f6d4
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-09-13 19:42:29 -0600

    commitWG1

--- ejericicios/ejercicio2.py
+++ ejericicios/ejercicio2.py
@@ -7,4 +7,36 @@ todo\ en PyQt5
 """
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QLabel, QPushButton
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QLineEdit, QLabel,
+                            QPushButton, QWidget, QVBoxLayout)
+
+class myAPP(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setGeometry(100,100,300,100)
+        self.setWindowTitle("Ejercicio 2")
+        
+        self.label = QLabel("Ingrese la clave:")
+        self.editxt = QLineEdit()
+        self.editxt.setEchoMode(QLineEdit.Password)
+        self.btn = QPushButton("Ingresar")
+        self.btn.clicked.connect(self.clicked_btn)
+        
+        central = QWidget()
+        layout = QVBoxLayout()
+        
+        layout.addWidget(self.label)
+        layout.addWidget(self.editxt)
+        layout.addWidget(self.btn)
+        central.setLayout(layout)
+        self.setCentralWidget(central)
+        
+    def clicked_btn(self):
+        clave = self.editxt.text()
+        print(f"La clave es: {clave}")
+
+app = QApplication(sys.argv)
+window = myAPP()
+window.show()
+app.exec()
