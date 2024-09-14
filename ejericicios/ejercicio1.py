d69ef1210d689625a791aa008ba2a7ca34ebb65a
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-09-13 19:22:55 -0600

    commitWG

--- ejericicios/ejercicio1.py
+++ ejericicios/ejercicio1.py
@@ -6,20 +6,27 @@
 todo\ usando PyQt5
 """
 
+# se importan los módulos necesarios para crear una aplicación gráfica con PyQt5
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+
+# se define la clase que hereda de QMainWindow
 
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        # se configura la ventana principal
         self.setGeometry(100, 100, 300, 275)
         self.setWindowTitle("Ejercicio 1")
         
+        # Esta etiqueta se utilizará para mostrar el nombre y la edad en la ventana
         label = QLabel("William Josue Guandique Rivera, 18 años")
         
+        # Se establece la etiqueta creada como el widget central de la ventana principal utilizando el método setCentralWidget
         self.setCentralWidget(label)
 
+# se muestra la aplicacion
 app = QApplication(sys.argv)
 window = MyApp()
 window.show()
---
+++ ejericicios/ejercicio2.py
@@ -0,0 +1,10 @@
+"""
+* Ejercicio 2
+
+? Construir un programa que muestre una ventana y que lea
+! una clave secreta sin mostra los caracteres que la componen,
+todo\ en PyQt5
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit, QLabel, QPushButton
