f5d9fdcb6e0035f60454b6b245e6bc69a66ab6fe
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-09-15 13:23:53 -0600

    commit BS

--- ejericicios/ejercicio3.py
+++ ejericicios/ejercicio3.py
@@ -10,25 +10,26 @@ __________________________________________________________
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit,
+                            QTextEdit, QPushButton, QVBoxLayout)
 
 class Ventana(QWidget):
     def __init__(self):
         super().__init__()
         
         # Configuramos nuestra ventana
+        self.label = QLabel("Ingrese los siguientes datos: \n")
         self.setWindowTitle("Formulario de Cédula y Nombre")
         self.setGeometry(100, 100, 300, 200)
         
         # Creamos los elementos dde nuestra interfaz
         self.label_cedula = QLabel("Número de Cédula:")
         self.input_cedula = QLineEdit(self)
-        self.input_cedula.setText("06784507-1")  # numero de la cédula que se mostrara
         
         self.label_nombre = QLabel("Nombre Completo:")
         self.input_nombre = QLineEdit(self)
-        self.input_nombre.setText("Briseily Yamileth Solorzano Hernandez")  # el nombre que se mostrara
         
+        self.txt_data = QTextEdit()
         self.boton = QPushButton("Mostrar Datos", self)
         
         # Conectamos el botón para la función que nos mostrara los datos
@@ -36,11 +37,13 @@ class Ventana(QWidget):
         
         # Creamos un layout vertical donde se almacenaran los elementos
         layout = QVBoxLayout()
+        layout.addWidget(self.label)
         layout.addWidget(self.label_cedula)
         layout.addWidget(self.input_cedula)
         layout.addWidget(self.label_nombre)
         layout.addWidget(self.input_nombre)
         layout.addWidget(self.boton)
+        layout.addWidget(self.txt_data)
         
         # Asignamos el layout a nuestra ventana
         self.setLayout(layout)
@@ -48,7 +51,16 @@ class Ventana(QWidget):
     def mostrar_datos(self):
         cedula = self.input_cedula.text()
         nombre = self.input_nombre.text()
-        print(f"Cédula: {cedula}, Nombre Completo: {nombre}")
+        
+        if not cedula and not nombre:
+            self.txt_data.setText("Por favor ingrese su número de cédula y su nombre")
+        elif not cedula:
+            self.txt_data.setText("Por favor ingrese su número de cédula")
+        elif not nombre:
+            self.txt_data.setText("Por favor ingrese su nombre completo")
+        else:
+            print(f"Cédula: {cedula}, Nombre Completo: {nombre}")
+            self.txt_data.setText(f"Cédula: {cedula}\nNombre Completo: {nombre}")
 
 # Creamos la aplicación
 if __name__ == "__main__":
